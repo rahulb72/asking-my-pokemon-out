d2420851f9dd28e85809729a43dc1f417c731c26
rahulb72 <rockbhagat72@gmail.com>
2025-02-11 14:49:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-function moveRandomE1(elm) {
+let speed = 80;
+let attempts = 0;
+
+function moveRandomEl(elm) {
     elm.style.position = "absolute";
-    elm.style.top = Math.floor(Math.random() * 90 + 5) + "%";
-    elm.style.left = Math.floor(Math.random() * 90 + 5) + "%";
+    elm.style.top = Math.floor(Math.random() * speed) + "%";
+    elm.style.left = Math.floor(Math.random() * speed) + "%";
+
+    speed -= 10;
+    if (speed < 30) {
+        speed = 80;
+    }
 }
 
 const moveRandom = document.querySelector("#move-random");
 
-moveRandom.addEventListener("mouseenter", function(e) {
-    moveRandomE1(e.target);
-});
+if (moveRandom) {
+    function moveNoButton(e) {
+        attempts++;
+        if (attempts > 5) {
+            alert("You have no choice but to say YES! 😜");
+        }
+        moveRandomEl(e.target);
+    }
+
+    moveRandom.addEventListener("mouseenter", moveNoButton);
+    moveRandom.addEventListener("touchstart", moveNoButton);
+}
